a7d59bb565f0ede3639cc6610e54bf935642301a
Mohammed Fayaj <34102787+MohammedFayaj@users.noreply.github.com>
2019-05-30 14:57:20 -0400

    Add files via upload

---
+++ Python/numbers/Exercis_1.py
@@ -0,0 +1,5 @@
+cost_per_hour = 0.51
+cost_per_day = 24 * cost_per_hour
+cost_per_month = 30 * cost_per_day
+print('Cost to operate one server per day is $ {}'.format(cost_per_day))
+print('Cost to operate one server per month is $ {}'.format(cost_per_month))
